8a84bcf1004d9834b02e5c8601924343e47eab93
Hayden Gillanders <hg234@students.waikato.ac.nz>
2023-09-07 08:51:34 +1200

    input source functionality (incomplete)

--- Circuits/InputSource.cs
+++ Circuits/InputSource.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * NAME: Hayden Gillanders
+ * ID NUMBER: 1636274
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +13,12 @@ namespace Circuits
 {
     class InputSource : Gate
     {
+        public InputSource(int x, int y)
+        {
+            pins.Add(new Pin(this, false, 20));
+
+            MoveTo(x, y);
+
+        }
     }
 }
