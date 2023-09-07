e58641827419bf3104e9e2da409ea0dcda98f67f
Hayden Gillanders <hg234@students.waikato.ac.nz>
2023-09-08 11:49:53 +1200

    statr compound

---
+++ Circuits/Compound.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circuits
+{
+    public class Compound : Gate
+    {
+        List<Gate> gates = new List<Gate>();
+
+        public static void operator + (List<Gate> gates, Gate gate)
+        {
+            gates.Add(gate);
+            return gates;
+        }
+    }
+}
