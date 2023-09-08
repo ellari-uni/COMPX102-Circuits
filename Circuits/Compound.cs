32d5710a14c82c02187c2af4c5725096094a6d75
Hayden Gillanders <hg234@students.waikato.ac.nz>
2023-09-08 12:45:36 +1200

    Eval compound

--- Circuits/Compound.cs
+++ Circuits/Compound.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,30 @@ namespace Circuits
     {
         List<Gate> gates = new List<Gate>();
 
-        public static void operator + (List<Gate> gates, Gate gate)
+        public static List<Gate> operator + (Compound cmd, Gate gate)
         {
-            gates.Add(gate);
-            return gates;
+            cmd.gates.Add(gate);
+            return cmd.gates;
+        }
+
+        public override void Draw(Graphics paper)
+        { 
+            foreach(Gate gate in gates)
+            {
+                gate.Draw(paper);
+            }
+        }
+
+        public override bool Evaluate()
+        {
+            List<OutputLamp> lamps = new List<OutputLamp>();
+            foreach (Gate gate in gates)
+            {
+                if (gate is OutputLamp lamp) lamps.Add(lamp);
+            }
+
+            foreach (Gate gate in gates) if (gate is OutputLamp lamp) lamps.Add(lamp);
+
         }
     }
 }
